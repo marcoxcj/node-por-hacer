5669de2f60a0f92d8bce074a4420f1d592bde86c
marco <marco.bryan00@gmail.com>
2019-09-23 10:12:43 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## aplicacion de comandos
+
+Este es un ejercicio de comandos
+
+Recuerden instalar los paquetes de node 
+
+...
+npm install
+...
---
+++ config/yargs.js
@@ -0,0 +1,35 @@
+const opts = {
+    descripcion: {
+        demand: true,
+        alias: 'd',
+        desc: 'Descripcion de la tarea por hacer'
+    },
+    completado: {
+        alias: 'c',
+        default: true,
+        desc: 'Marca como completado o pendiente la tarea'
+    }
+}
+
+const argv = require('yargs')
+    .command('actualizar', 'Actualiza el estado completado de una tarea', opts)
+    .command('crear', 'Crea un elemento por hacer', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripcion de la tarea por hacer'
+        }
+    })
+    .command('borrar', 'Elimina un elemento', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripcion de la tarea por hacer'
+        }
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
